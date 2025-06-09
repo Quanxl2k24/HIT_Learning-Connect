89a643b0c97908a9b641fc76c74e713a801276cb
Vuong <vanvuong2005827@gmail.com>
2025-06-09 19:38:45 +0700

    add: project

---
+++ backend/hit-learning-connect-ver1/src/main/java/org/example/hitlearningconnectver1/HitLearningConnectVer1Application.java
@@ -0,0 +1,13 @@
+package org.example.hitlearningconnectver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitLearningConnectVer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitLearningConnectVer1Application.class, args);
+    }
+
+}
