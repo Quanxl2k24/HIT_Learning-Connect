c308d2c9aa757e4be5a02f246de0f7e9502bad6b
Quanxl2k24 <quanxl2k24@gamil.com>
2025-07-25 23:56:20 +0700

    fix:vite-config

--- react-fontend/vite.config.js
+++ react-fontend/vite.config.js
@@ -30,7 +30,7 @@
 
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
